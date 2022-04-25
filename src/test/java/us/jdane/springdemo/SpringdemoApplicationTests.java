1e2a04c5cb1c2569a134c81989f2799ce97b1e0c
Joshua Dane <joshrdane@gmail.com>
2022-04-25 08:43:40 -0400

    Initial commit

---
+++ src/test/java/us/jdane/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package us.jdane.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
